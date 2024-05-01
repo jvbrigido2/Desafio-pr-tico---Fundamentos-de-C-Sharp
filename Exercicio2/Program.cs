60a7bf09e0612b16b42ae9c5dcc59d42185f62f1
Joao Brigido <jv_brigido@hotmail.com>
2024-05-01 01:15:26 -0300

    feat: Adiciona o Exercicio 2

---
+++ Exercicio2/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Write("Enter your first name: ");
+var firstName = Console.ReadLine();
+Console.Write("Enter your last name: ");
+var  lastName = Console.ReadLine();
+
+Console.WriteLine($"Your fullname is {firstName} {lastName}.");
+Console.ReadKey();

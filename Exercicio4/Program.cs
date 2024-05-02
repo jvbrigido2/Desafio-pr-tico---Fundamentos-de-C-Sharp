6738fd7d71c9fe691bfa5643883f1db6607669cb
Joao Brigido <jv_brigido@hotmail.com>
2024-05-02 09:50:30 -0300

    feat: Adiciona o Exercicio 4.

---
+++ Exercicio4/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Digite uma ou mais palavras separadas por espaços:");
+
+string input = Console.ReadLine();
+
+string[] palavras = input.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+
+foreach (string palavra in palavras)
+{
+    Console.WriteLine($"A palavra '{palavra}' tem {palavra.Length} caracteres.");
+}
+
+Console.ReadKey();
